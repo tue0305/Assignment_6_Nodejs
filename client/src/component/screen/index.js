c17ccb55287426c577df6f4f7524add17392cf7a
phat181999 <phat181999@gmail.com>
2021-12-16 16:21:47 -0800

    [Layout-page-main]

--- client/src/component/screen/index.js
+++ client/src/component/screen/index.js
@@ -1,11 +1,13 @@
 import React, { Fragment }  from 'react';
 import Header from './header/header';
 import Footer from './footer/footer';
+import FlashSale from '../user/flash-Sale/flashSale';
 
 export default function Index() {
     return (
         <Fragment>
             <Header/>
+            <FlashSale/>
             <Footer/>
         </Fragment>
     )
--- client/src/component/screen/navbar/navbar.js
+++ client/src/component/screen/navbar/navbar.js
@@ -13,7 +13,6 @@ import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import PhoneIcon from '@material-ui/icons/Phone';
 
