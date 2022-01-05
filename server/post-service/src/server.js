bfdead63660d0ca6fa4f36ff9e5a7d6af4ef7f7d
phat181999 <phat181999@gmail.com>
2022-01-04 16:06:27 -0800

    [DROP DATABASE]

--- server/post-service/src/server.js
+++ server/post-service/src/server.js
@@ -3,11 +3,14 @@ const { PORT } = require("./config/config");
 const { DB_Connection } = require("./database/index");
 const expressApp = require("./express_app");
 const { createChannel } = require("./utils");
+const crawlData = require("./crawler/index");
 
 const StartServer = async () => {
   const app = express();
 
   await DB_Connection();
+
+  await crawlData();
 
   const channel = await createChannel();
 
